ba322b491f7ff8dbcbf91493d6641efdbdfb6b0c
junseong <wnstjd_97@naver.com>
2023-08-23 23:11:56 +0900

    28074 모비스.

--- acmicpc/28074 모비스.py
+++ acmicpc/28074 모비스.py
@@ -1,26 +1,13 @@
-word = input()
 MOBIS = ["M", "O", "B", "I", "S"]
+word = input()
 result = True
+
 for i in MOBIS:
     if i not in word:
         result = False
         break
 
 if result:
-    print("Yes")
+    print("YES")
 else:
-    print("No")
-
-# MOBIS = ["M", "O", "B", "I", "S"]
-# word = input()
-# result = True
-
-# for i in MOBIS:
-#     if i not in word:  # MOBIS가 없으면 False로 변경 후 break
-#         result = False
-#         break
-
-# if result:
-#     print("YES")
-# else:
-#     print("NO")
+    print("NO")
