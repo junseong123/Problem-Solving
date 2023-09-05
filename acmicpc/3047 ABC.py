73ea314db88724cf22feaea69c6c69cbe554fffa
junseong <wnstjd_97@naver.com>
2023-09-05 17:49:50 +0900

    3047 ABC Python AC

--- acmicpc/3047 ABC.py
+++ acmicpc/3047 ABC.py
@@ -1,14 +1,15 @@
-x = list(map(int, input().split()))
-y = []
-p = input()
+# 입력
+a, b, c = map(int, input().split())
+order = input()
 
-for i in range(3):
-    if p[i] == "A":
-        print(min(x), end=" ")
-        x.remove(min(x))
-    elif p[i] == "C":
-        print(max(x), end=" ")
-        x.remove(max(x))
+# 오름차순 정렬
+a, b, c = sorted((a, b, c))
+
+# 순서대로 출력
+for i in order:
+    if i == "A":
+        print(a, end=" ")
+    elif i == "B":
+        print(b, end=" ")
     else:
-        b = x
-        print(b[0], end=" ")
+        print(c, end=" ")
