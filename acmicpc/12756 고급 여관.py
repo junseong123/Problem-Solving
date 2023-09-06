20e7c00fa015556c04e71d51355a025e4524cca0
junseong <wnstjd_97@naver.com>
2023-09-06 11:27:42 +0900

    12756 고급 여관 Python WA_1

--- acmicpc/12756 고급 여관.py
+++ acmicpc/12756 고급 여관.py
@@ -5,13 +5,13 @@ while True:
         break
     B[1] -= A[0]
     A[1] -= B[0]
-if A[1] < 0:
-    A[1] = 0
-elif B[1] < 0:
-    B[1] = 0
-if A[1] > B[1]:
+    if A[1] < 0:
+        A[1] = 0
+    elif B[1] < 0:
+        B[1] = 0
+if A[1] > 0:
     print("PLAYER A")
-elif A[1] < B[1]:
+elif 0 < B[1]:
     print("PLAYER B")
-else:
+elif A[1] == 0 and B[1] == 0:
     print("DRAW")
