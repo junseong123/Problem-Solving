38b5f960eaa72b407244d80ce18762d0f81efa76
junseong <wnstjd_97@naver.com>
2023-09-07 19:12:44 +0900

    9366 삼각형 분류 Python AC

--- acmicpc/9366 삼각형 분류.py
+++ acmicpc/9366 삼각형 분류.py
@@ -2,12 +2,13 @@ T = int(input())
 a = []
 for i in range(1, T + 1):
     a, b, c = sorted(map(int, input().split()))
-    if a == b == c:
+    if a + b <= c:
+        print(f"Case #{i}: invalid!")
+    elif a == b == c:
         print(f"Case #{i}: equilateral")
     elif a == b or c == a or b == c:
         print(f"Case #{i}: isosceles")
-    else:
-        if b**2 + a**2 == c**2:
-            print(f"Case #{i}: scalene")
-        else:
-            print(f"Case #{i}: invalid!")
+    elif b != a != c:
+        print(f"Case #{i}: scalene")
+
+# 삼각형의 결정조건 : 짧은변의 길이의 합 > 긴변
