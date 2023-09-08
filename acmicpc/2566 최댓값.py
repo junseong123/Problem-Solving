428ec9101ea5c9946d1589380ccacdf8d9444a1b
junseong <wnstjd_97@naver.com>
2023-09-08 11:09:03 +0900

    2566 최댓값 Python RTE_1

--- acmicpc/2566 최댓값.py
+++ acmicpc/2566 최댓값.py
@@ -3,11 +3,12 @@ max = 0
 for i in range(9):
     a.append(list(map(int, input().split())))
 
-for i in range(9):
-    for j in range(9):
-        if a[i][j] > max:
-            max = a[i][j]
-            x = i
-            y = j
+for q in range(9):
+    for w in range(9):
+        if a[q][w] > max:
+            max = a[q][w]
+            x = q
+            y = w
 print(max)
 print(x + 1, y + 1)
+print(a[2][1])
